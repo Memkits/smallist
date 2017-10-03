9fd2f1021524c0ca824088b5b53ecb5d790a95a5
jiyinyiyong <jiyinyiyong@gmail.com>
2017-10-03 13:00:31 +0800

    config CDN

--- src/app/render.cljs
+++ src/app/render.cljs
@@ -6,7 +6,7 @@
             [app.schema :as schema]))
 
 (def base-info
-  {:title "CoWorkflow", :icon "http://logo.mvc-works.org/mvc.png", :ssr nil, :inner-html nil})
+  {:title "Smalllist", :icon "http://repo-cdn.b0.upaiyun.com/logo/mvc-works.png", :ssr nil})
 
 (defn dev-page []
   (make-page
@@ -15,18 +15,24 @@
     base-info
     {:styles [], :scripts ["/main.js" "/browser/lib.js" "/browser/main.js"]})))
 
+(def preview? (= "preview" js/process.env.prod))
+
 (defn prod-page []
   (let [html-content (make-string (comp-container schema/store))
         manifest (.parse js/JSON (slurp "dist/assets-manifest.json"))
-        cljs-manifest (.parse js/JSON (slurp "dist/manifest.json"))]
+        cljs-manifest (.parse js/JSON (slurp "dist/manifest.json"))
+        cdn (if preview? "" "http://repo-cdn.b0.upaiyun.com/smallist/")
+        prefix-cdn (fn [x] (str cdn x))]
     (make-page
      html-content
      (merge
       base-info
-      {:styles [(aget manifest "main.css")],
-       :scripts [(aget manifest "main.js")
-                 (-> cljs-manifest (aget 0) (aget "js-name"))
-                 (-> cljs-manifest (aget 1) (aget "js-name"))],
+      {:styles [(prefix-cdn (aget manifest "main.css"))],
+       :scripts (map
+                 prefix-cdn
+                 [(aget manifest "main.js")
+                  (-> cljs-manifest (aget 0) (aget "js-name"))
+                  (-> cljs-manifest (aget 1) (aget "js-name"))]),
        :ssr "respo-ssr"}))))
 
 (defn main! []

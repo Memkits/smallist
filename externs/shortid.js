6eadc86440f3c66c6c751dfcb3130cba8b1a9fee
jiyinyiyong <jiyinyiyong@gmail.com>
2017-10-07 01:45:42 +0800

    shorter externs

--- externs/shortid.js
+++ externs/shortid.js
@@ -2,5 +2,7 @@
  * @externs
  */
 
-var module$node_modules$shortid$index = {};
-module$node_modules$shortid$index.generate = function() {};
+/** @constructor */
+var X = function() {};
+
+X.prototype.generate;
